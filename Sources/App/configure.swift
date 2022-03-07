a5ba50b9d7dcaede1f9719ebd1d42f5aa22d08d8
Simon Schöpke <simon2204s@gmail.com>
2022-03-07 14:30:45 +0100

    .

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,7 +3,7 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) throws {
     // set public directory to a custom one
-    app.directory.resourcesDirectory = "/Users/simon/Desktop/TestbenchDirectories"
+    app.directory.resourcesDirectory = "/Users/simon/Documents/WMI/TestbenchDirectories"
      
     // enable file middleware
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.resourcesDirectory))
